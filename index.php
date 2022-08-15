5deeae283c6671f2eefc0d6d01c767db57e7e111
maulana270601 <maulanaalkhairat123@gmail.com>
2022-08-15 14:04:37 +0800

    penambahan ul dan li

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt tempora architecto quia consequuntur laborum fuga.</p>
+    <ul>
+        <li>Maulana Malik Ibrahim</li>
+        <li>mengkelaim dirinya ganteng</li>
+    </ul>
 </body>
 </html>
