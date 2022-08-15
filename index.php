96c42422f7551ec7b81f70ff658400e0ffdb1122
maulana270601 <maulanaalkhairat123@gmail.com>
2022-08-15 13:53:43 +0800

    penambahan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COBA GUT REMOTE/title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt tempora architecto quia consequuntur laborum fuga.</p>
+</body>
+</html>
